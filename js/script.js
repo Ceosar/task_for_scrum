1b75b054220fe6e21f563225d886f2362e6786c2
Daniil <krupnov_2001@mail.ru>
2023-05-19 10:07:45 +0300

    ready-to-check

---
+++ js/script.js
@@ -0,0 +1,37 @@
+function confirmChoosing(){
+    let voteOfGreen = document.getElementById('voteGreen').value;
+    let voteOfRed = document.getElementById('voteRed').value;
+    let voteOfBlue = document.getElementById('voteBlue').value;
+
+    if(voteOfBlue === voteOfGreen && voteOfGreen === voteOfRed && voteOfRed === voteOfGreen){
+        document.getElementsByClassName('start-to-vote')[0].style.display='none';
+        document.getElementById('enterRes').textContent = (voteOfBlue);
+        document.getElementsByClassName('lastEnter')[0].style.display='block';
+    }
+    else{
+        document.getElementsByClassName('tryAgain')[0].style.display='block';
+        showComments(voteOfGreen, voteOfRed, voteOfBlue);
+    }
+}
+
+function showComments(paramGreen, paramRed, paramBlue){
+    document.getElementsByClassName('lastEnter')[0].style.display='none';
+    document.getElementsByClassName('btn-conf-choose')[0].style.display='none';
+    document.getElementsByClassName('start-to-vote')[0].style.display='none';
+
+    let textCommentGreen = document.getElementsByClassName('person-comment-green')[0].value;
+    let textCommentRed = document.getElementsByClassName('person-comment-red')[0].value;
+    let textCommentBlue = document.getElementsByClassName('person-comment-blue')[0].value;
+
+    document.getElementById('green-score').textContent = (paramGreen);
+    document.getElementById('red-score').textContent = (paramRed);
+    document.getElementById('blue-score').textContent = (paramBlue);
+
+    document.getElementById('green-text').textContent = (textCommentGreen);
+    document.getElementById('red-text').textContent = (textCommentRed);
+    document.getElementById('blue-text').textContent = (textCommentBlue);
+}
+
+function tryAgainFun(){
+    location.reload();
+}
